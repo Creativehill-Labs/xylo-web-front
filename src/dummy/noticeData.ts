7f2a60f521e5a255f89fbf13735c1f378caf052e
kkangtaeng <kimtaeng516@gmail.com>
2022-11-21 16:23:37 +0900

    feat: notice용 더미 데이터 생성

---
+++ src/dummy/noticeData.ts
@@ -0,0 +1,101 @@
+export const articleData = [
+  { id: 1, title: `Revision of Terms and Conditions 이용약관 개정 안내` },
+  {
+    id: 2,
+    title: `1st 100% of Mining Asset Transfer Promotion Confirmation Deadline`,
+  },
+  { id: 3, title: `Display Error After 1st Mining Asset Transfer` },
+  { id: 4, title: `Promotion Related Inquiries` },
+  {
+    id: 5,
+    title: `Notice for 1st Mining Asset Transfer Promotion Applicants (100% of Assets)`,
+  },
+  {
+    id: 6,
+    title: `Temporary In-App Function Suspension Notice and Personal Security Management Request`,
+  },
+  {
+    id: 7,
+    title: `Information on temporary deactivation of accounts suspected of being hacked`,
+  },
+  { id: 8, title: `Delay on Reward Payment / [공지] 리워드 지급 지연 안내` },
+  {
+    id: 9,
+    title: `[Policy Discussion] Adjustment of SO delegation withdrawal and change penalty / [정책변경 토론] SO위임 철회 및 변경 페널티 관련 조정 건`,
+  },
+  {
+    id: 10,
+    title: `Temporary In-App Function Suspension Notice and Personal Security Management Request`,
+  },
+  {
+    id: 11,
+    title: `Announcing Whitepaper V0.9.7 update`,
+  },
+  {
+    id: 12,
+    title: ` Roadmap Announcement`,
+  },
+  {
+    id: 13,
+    title: `Bridge Bidirectional Service Launch Notice 브릿지 양방향 서비스 런칭 안내`,
+  },
+  {
+    id: 14,
+    title: `[Policy Discussion] Adjustment of SO delegation withdrawal and change penalty `,
+  },
+  {
+    id: 15,
+    title: `Delayed on Reward Payment / [공지] 리워드 지급 지연 및 지급 완료 안내`,
+  },
+  {
+    id: 16,
+    title: `Temporary In-App Function Suspension Notice and Personal Security Management Request`,
+  },
+  {
+    id: 17,
+    title: `qweqwe`,
+  },
+  {
+    id: 18,
+    title: `asdasd`,
+  },
+  {
+    id: 19,
+    title: `zxczxc`,
+  },
+];
+
+export const promotedData = [
+  {
+    id: 1,
+    title: `[Policy Discussion] Adjustment of SO delegation withdrawal and change penalty / [정책변경 토론] SO위임 철회 및 변경 페널티 관련 조정 건`,
+  },
+  {
+    id: 2,
+    title: `Temporary In-App Function Suspension Notice and Personal Security Management Request`,
+  },
+  {
+    id: 3,
+    title: `Announcing Whitepaper V0.9.7 update`,
+  },
+  {
+    id: 4,
+    title: ` Roadmap Announcement`,
+  },
+  {
+    id: 5,
+    title: `Bridge Bidirectional Service Launch Notice 브릿지 양방향 서비스 런칭 안내`,
+  },
+  {
+    id: 6,
+    title: `[Policy Discussion] Adjustment of SO delegation withdrawal and change penalty `,
+  },
+  {
+    id: 7,
+    title: `Delayed on Reward Payment / [공지] 리워드 지급 지연 및 지급 완료 안내`,
+  },
+  {
+    id: 8,
+    title: `Temporary In-App Function Suspension Notice and Personal Security Management Request`,
+  },
+];
