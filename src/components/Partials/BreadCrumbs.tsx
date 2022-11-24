84677b60d63176964e870a68631f92be26dc7ba1
kkangtaeng <kimtaeng516@gmail.com>
2022-11-24 14:33:05 +0900

    fix: BreadCrumbs 코드 수정

--- src/components/Partials/BreadCrumbs.tsx
+++ src/components/Partials/BreadCrumbs.tsx
@@ -1,30 +1,66 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useLocation, Link } from 'react-router-dom';
 import Flex from '../Flex';
 import Text from '../Text';
 import Box from '../Box';
 
-const Button = styled.button<{ color?: string; background?: string }>`
+const LinkButton = styled.button<{ color?: string; background?: string }>`
   padding: 12px 20px;
   border: none;
   border-radius: 999px;
-  color: ${({ color }) => color};
   background-color: ${({ background }) => background};
   cursor: pointer;
 `;
 
-const BreadCrumbs: FC = () => {
+const BreadCrumbs: FC<{ search?: boolean }> = ({ search }) => {
+  const [firstPath, setFirstPath] = useState(``);
+  const [secondPath, setSecondPath] = useState(``);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const path = pathname.split(`/`);
+    setFirstPath(path[2]);
+    setSecondPath(path[3]);
+  }, [pathname]);
+
   return (
     <Flex alignItems="center" margin="80px 0 60px 0">
-      <Button color="#fff" background="#000">
-        <Text size="16px">Help Center</Text>
-      </Button>
-      <Box margin="0 16px">/</Box>
-      <Button color="#000" background="#aae112">
-        <Text size="16px">Notice</Text>
-      </Button>
-      <Box margin="0 16px">/</Box>
-      <Text size="16px">Policy Discussion</Text>
+      {search ? (
+        <>
+          <LinkButton background="#000">
+            <Link to="/helpcenter/faq">
+              <Text size="16px">Help Center</Text>
+            </Link>
+          </LinkButton>
+          <Box margin="0 16px">/</Box>
+          <Text size="20px" color="#000">
+            Search results
+          </Text>
+        </>
+      ) : (
+        <>
+          <LinkButton background="#000">
+            <Link to="/helpcenter/faq">
+              <Text size="16px">Help Center</Text>
+            </Link>
+          </LinkButton>
+          <Box margin="0 16px">/</Box>
+          <LinkButton background="#aae112">
+            <Link to={`/helpcenter/${firstPath}`}>
+              <Text size="16px" color="#000">
+                {firstPath.replace(/\b[a-z]/g, (char) => char.toUpperCase())}
+              </Text>
+            </Link>
+          </LinkButton>
+          <Box margin="0 16px">/</Box>
+          <Link to={`/helpcenter/${firstPath}/${secondPath}`}>
+            <Text size="20px" color="#000">
+              {secondPath.replace(/\b[a-z]/g, (char) => char.toUpperCase())}
+            </Text>
+          </Link>
+        </>
+      )}
     </Flex>
   );
 };
